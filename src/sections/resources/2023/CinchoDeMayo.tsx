413177142f7dffcb2d5cac6d79f1b2d63f85451a
Patrick Hulin <patrick.hulin@gmail.com>
2024-11-12 20:46:49 -0500

    cincho tweaks.

--- src/sections/resources/2023/CinchoDeMayo.tsx
+++ src/sections/resources/2023/CinchoDeMayo.tsx
@@ -1,17 +1,19 @@
 import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
-import { totalFreeRests } from "kolmafia";
+import { myAscensions, totalFreeRests } from "kolmafia";
 import {
   $item,
   ChateauMantegna,
   CinchoDeMayo as LibramCincho,
   get,
   have,
+  MayamCalendar,
+  sumNumbers,
 } from "libram";
 
 import Line from "../../../components/Line";
 import Tile from "../../../components/Tile";
 import { haveUnrestricted } from "../../../util/available";
-import { plural } from "../../../util/text";
+import { commaAnd, plural } from "../../../util/text";
 
 const CinchoDeMayo = () => {
   const cinchoDeMayo = $item`Cincho de Mayo`;
@@ -21,8 +23,31 @@ const CinchoDeMayo = () => {
   const freeRestsRemaining = totalFreeRests() - freeRests;
 
   const totalCinch = LibramCincho.totalAvailableCinch();
-
   const possibleFiestaExits = Math.floor(totalCinch / 60);
+
+  const additionalCinchSources: Record<string, number> = {};
+  if (
+    haveUnrestricted($item`June cleaver`) &&
+    !have($item`mother's necklace`)
+  ) {
+    additionalCinchSources["mother's necklace"] = 25;
+  }
+  if (haveUnrestricted($item`Mayam Calendar`)) {
+    const chairAvailable =
+      MayamCalendar.remainingUses() > 0 && MayamCalendar.available("chair");
+    const resetAvailable = get("lastTempleAdventures") < myAscensions();
+    if (chairAvailable && resetAvailable) {
+      additionalCinchSources["Mayam chair plus reset chair"] = 50;
+    } else if (chairAvailable) {
+      additionalCinchSources["Mayam chair"] = 25;
+    } else if (resetAvailable) {
+      additionalCinchSources["Mayam chair after reset"] = 25;
+    }
+  }
+
+  const additionalCinch = sumNumbers(Object.values(additionalCinchSources));
+  const additionalFiestaExits =
+    Math.floor((totalCinch + additionalCinch) / 60) - possibleFiestaExits;
 
   if (totalCinch === 0) return null;
 
@@ -50,7 +75,7 @@ const CinchoDeMayo = () => {
           </Text>{" "}
           Force a NC on your next adventure. You have{" "}
           <Text as="b">{possibleFiestaExits}</Text> more possible, with{" "}
-          {totalCinch % 60}% cinch leftover.
+          {totalCinch % 60}% cinch left over.
         </Line>
       )}
       <UnorderedList>
@@ -77,14 +102,14 @@ const CinchoDeMayo = () => {
         You have {totalCinch}% more cinch available, accounting for your{" "}
         {plural(freeRestsRemaining, "remaining free rest")}.
       </Line>
-      {have($item`June cleaver`) && !have($item`mother's necklace`) && (
+
+      {Object.entries(additionalCinchSources).length > 0 && (
         <Line>
-          You do{" "}
-          <Text as="span" color="red.500">
-            not
-          </Text>{" "}
-          have a mother's necklace yet. Possible 25% more cinch
-          {totalCinch % 60 >= 35 ? ", which will give another Party Exit" : ""}.
+          Possible {additionalCinch}% more cinch from{" "}
+          {commaAnd(Object.keys(additionalCinchSources))},
+          {additionalFiestaExits > 0 &&
+            ` giving ${plural(additionalFiestaExits, "more Party Exit")} and`}{" "}
+          leaving {(totalCinch + additionalCinch) % 60}% left over.
         </Line>
       )}
     </Tile>

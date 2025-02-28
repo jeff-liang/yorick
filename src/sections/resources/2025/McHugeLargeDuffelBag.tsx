314ceaae44d6eda356bbb6f52d9002d41e7e13e3
Patrick Hulin <patrick.hulin@gmail.com>
2025-02-28 18:11:59 -0500

    MHL duffel should be a nag.

--- src/sections/resources/2025/McHugeLargeDuffelBag.tsx
+++ src/sections/resources/2025/McHugeLargeDuffelBag.tsx
@@ -2,26 +2,36 @@ import { $item, have } from "libram";
 import { FC } from "react";
 
 import Tile from "../../../components/Tile";
+import { NagPriority } from "../../../contexts/NagContext";
+import useNag from "../../../hooks/useNag";
 import { haveUnrestricted } from "../../../util/available";
 import { inventoryLink } from "../../../util/links";
 
 const McHugeLargeDuffelBag: FC = () => {
   const duffelBag = $item`McHugeLarge duffel bag`;
 
-  if (!haveUnrestricted(duffelBag) || have($item`McHugeLarge left ski`)) {
-    return null;
-  }
+  const haveDuffelBag = haveUnrestricted(duffelBag);
+  const duffelBagOpen = have($item`McHugeLarge left ski`);
 
-  return (
-    <Tile
-      header="McHugeLarge duffel bag"
-      imageUrl="/images/itemimages/skibag2.gif"
-      href={inventoryLink(duffelBag)}
-      linkEntireTile
-    >
-      Open your duffel bag for ski equipment!
-    </Tile>
+  useNag(
+    () => ({
+      id: "mchugelarge-duffel-bag-open-nag",
+      priority: NagPriority.LOW,
+      imageUrl: "/images/itemimages/skibag2.gif",
+      node: haveDuffelBag && !duffelBagOpen && (
+        <Tile
+          header="McHugeLarge duffel bag"
+          imageUrl="/images/itemimages/skibag2.gif"
+          href={inventoryLink(duffelBag)}
+          linkEntireTile
+        >
+          Open your duffel bag for ski equipment!
+        </Tile>
+      ),
+    }),
+    [duffelBag, duffelBagOpen, haveDuffelBag],
   );
+  return null;
 };
 
 export default McHugeLargeDuffelBag;

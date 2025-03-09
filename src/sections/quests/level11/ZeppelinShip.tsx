0d40e6477751473333d5808e580cc5d0f7da0c20
Patrick Hulin <patrick.hulin@gmail.com>
2025-03-08 22:00:28 -0500

    Add zeppelinProgress reporting.

--- src/sections/quests/level11/ZeppelinShip.tsx
+++ src/sections/quests/level11/ZeppelinShip.tsx
@@ -1,9 +1,11 @@
 import { myPath } from "kolmafia";
-import { $item, $path, get, have } from "libram";
+import { $item, $path, clamp, get, have } from "libram";
 
 import Line from "../../../components/Line";
+import { plural } from "../../../util/text";
 
 const ZeppelinShip = () => {
+  const remaining = clamp(6 - get("zeppelinProgress"), 0, 6);
   return (
     <>
       {!have($item`Red Zeppelin ticket`) && (
@@ -11,12 +13,15 @@ const ZeppelinShip = () => {
           Purchase a red zeppelin ticket in the black market.
         </Line>
       )}
-      {have($item`glark cable`) && (
-        <Line color="red">Use glark cable in combat.</Line>
-      )}
-      <Line>Search for Ron in the Zeppelin.</Line>
+      <Line>
+        Search for Ron in the Zeppelin. Defeat{" "}
+        {plural(remaining, "more monster")}.
+      </Line>
       {myPath() !== $path`Avant Guard` && (
-        <Line>{get("_glarkCableUses")}/5 glark cables used. (free kills)</Line>
+        <Line>
+          {have($item`glark cable`) && "Use glark cable in combat. "}
+          {get("_glarkCableUses")}/5 glark cables used. (free kills)
+        </Line>
       )}
     </>
   );
